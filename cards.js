efb2f2c9c031886011042a65468cbb62bfd28c15
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-23 08:20:44 -0600

    had trouble with the delete function. finally made it work by adding EL to newBtn function

--- cards.js
+++ cards.js
@@ -7,30 +7,44 @@
 let create = document.getElementById("create");
 let input = document.getElementById("input");
 let output = document.getElementById("output");
+//let deleteBtn = document.createElement("button");
 
 create.addEventListener("click", function(){
     document.getElementById("output").innerHTML = input.value;
     newBtn();
     newDiv();
-})
+
+    
+
+});
+
+
+
 
 function newBtn () {
     let deleteBtn = document.createElement("button");
     let box = document.createTextNode("Delete");
     deleteBtn.appendChild(box);
     document.body.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", remove);
+
+
 }
 
 function newDiv () {
     let newDiv = document.createElement("div");
     newDiv.id = "divBox";
     newDiv.innerHTML = "made div";
-    newDiv.style.height = "50px";
-    newDiv.style.width = "200px";
+    newDiv.style.height = "150px";
+    newDiv.style.width = "500px";
     newDiv.style.backgroundColor = "red";
     document.body.appendChild(newDiv);
 }
 
+function remove(event) {
+    let div = document.getElementById("divBox");
+    document.body.removeChild(div);
+}
 
 
 
